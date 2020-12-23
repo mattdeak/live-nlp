126e0347db0a6e2b68e2680c9d28c3b54608d876
Matthew Deakos <mwoolnerdeakos@gmail.com>
2020-12-23 18:16:50 -0400

    Made JSONWriter atomic

--- reddit/writers.py
+++ reddit/writers.py
@@ -1,12 +1,17 @@
 import json
+import safer
 
 
 class JSONWriter:
     def __init__(self, outfile_path, comment_attributes=['body','created_utc','id','score']):
-        self.outfile = open(outfile_path, "a")
+        self.outfile_path = outfile_path
         self.comment_attributes = comment_attributes
 
     def run(self, data):
         data['comment'] = {key: getattr(data['comment'], key) for key in self.comment_attributes}
         assert type(data) == dict, "JSONWriter requires DICT input"
-        json.dump(data, self.outfile)
+        # Assures atomic write operation.
+        with safer.open(self.outfile_path, 'a') as outfile:
+            json.dump(data, outfile)
+            outfile.write('\n')
+
