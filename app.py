d56cabc526d9683db7d90f31942750dd8fb54085
Matthew Deakos <mwoolnerdeakos@gmail.com>
2020-12-23 15:32:34 -0400

    minor cleanup

--- app.py
+++ app.py
@@ -1,37 +1,27 @@
-import logging
-from bots import RedditStreamer
-from reddit.preprocessors import SimplePreprocessor, CommentBodyExtractor
-from analyzers import SimpleTopicModel, KerasModel, DummyAnalyzer, TFModelClient
-from livebot import LiveBot
-import sys
 import asyncio
+import logging
+import sys
+
 import tensorflow as tf
 import tensorflow_text as text
 
+from analyzers import (DummyAnalyzer, KerasModel, SimpleTopicModel,
+                       TFModelClient)
+from bots import RedditStreamer
+from reddit.preprocessors import CommentBodyExtractor, SimplePreprocessor
+
 logger = logging.getLogger('app')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stderr)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter(  
            '%(created)f:%(levelname)s:%(name)s:%(module)s:%(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 if __name__ == '__main__':
-    # scraper = SubmissionScraper('khwq8w', only_top_level_comments=False)
     preprocessor = CommentBodyExtractor()
     analyzer = TFModelClient(8501, 'BERT_test1')
-    # preprocessor = SimplePreprocessor()
-    # analyzer = SimpleTopicModel()
-
-    # bert_sentiment_model = tf.keras.models.load_model('models/test_model_BERT')
-    # analyzer = KerasModel(bert_sentiment_model, return_comments=True)
-    # simple_livebot = LiveBot(scraper, preprocessor, analyzer, refresh_rate=1)
 
     bot = RedditStreamer('news', preprocessor, analyzer, None)
     bot.run()
-
-
-
-
-
